ba2d015da87101a30c1defcd209945246f3f7c5d
magdarivas <70529545+magdarivas@users.noreply.github.com>
2025-10-17 17:04:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,24 +7,20 @@ async function datos(raw) {
         console.log(trabajos);
         trabajos.forEach((trabajo) => {
             portfolio.innerHTML += `
-
-                            <div class="col">
-                                <div class="card shadow-sm">
-                                <img src="${trabajo.imagen}" class"card-img-top">
-                                <div class="card-body">
-                                <p class="card-text">${trabajo.title}</p>
-                                <p class="card-text">${trabajo.description}</p>
-                                <div class="d-flex
-                                justify-content-between
-                                align-items-center">
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <img src="${trabajo.imagen}" class="card-img-top">
+                        <div class="card-body">
+                            <p class="card-text">${trabajo.title}</p>
+                            <div class="d-flex justify-content-between align-items-center">
                                 <div class="btn-group">
-                                <button type="button" class="btn btn-sm btn-outline-secondary">${trabajo.categoria}</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">${trabajo.categoria}</button>
                                 </div>
-                                <small class="text-body-secondary">Reciente </small>
-             </div>
-             </div>
-            </div>
-            </div>`;
+                                <small class="text-body-secondary">Reciente</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
         });
     } catch (error) {
         console.error("Error al cargar los datos:", error);
